38039b967f627dac770d18295d85c86d4756bd64
fruela-perez <fruela.perez@protonmail.com>
2020-02-16 17:09:43 +0100

    "\n" --> PHP_EOL

--- print.php
+++ print.php
@@ -1,20 +1,21 @@
 <?php
 	$archivoLogo     = $argv [ 1 ];
 
+	if ( ! file_exists ( $archivoLogo ) ) die ();
+
 	$cmdAnchoLinea   = 'cat ' . $archivoLogo . ' | awk \'{print length, $0}\' | sort -nr | head -1 | cut -d \' \' -f 1';
-	$anchoLinea      = (int) str_replace ( "\n", "", shell_exec ( $cmdAnchoLinea ) );
-	$anchoPantalla   = (int) str_replace ( "\n", "", shell_exec ( 'tput cols' ) );
+	$anchoLinea      = (int) str_replace ( PHP_EOL, "", shell_exec ( $cmdAnchoLinea ) );
+	$anchoPantalla   = (int) str_replace ( PHP_EOL, "", shell_exec ( 'tput cols' ) );
 	$agregarEspacios = ( $anchoPantalla - $anchoLinea ) / 2;
 	
-
-	echo "\n";
-
 	$fn = fopen ( $archivoLogo , "r" );
+
+	echo PHP_EOL;
 
 	while ( ! feof ( $fn ) )  
 	{
 		echo str_repeat ( " ", $agregarEspacios ) . fgets ( $fn );
 	}
 
-	echo "\n";
+	echo PHP_EOL;
 ?>
