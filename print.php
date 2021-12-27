3a0f7f117f33e63caedd1da5f0d2480488b3db68
Fruela Pérez <fruela.perez@protonmail.com>
2021-12-27 19:29:23 +0100

    Reemplazada / por intdiv()

--- print.php
+++ print.php
@@ -6,7 +6,7 @@
 	$cmdAnchoLinea   = 'cat ' . $archivoLogo . ' | awk \'{print length, $0}\' | sort -nr | head -1 | cut -d \' \' -f 1';
 	$anchoLinea      = (int) str_replace ( PHP_EOL, "", shell_exec ( $cmdAnchoLinea ) );
 	$anchoPantalla   = (int) str_replace ( PHP_EOL, "", shell_exec ( 'tput cols' ) );
-	$agregarEspacios = ( $anchoPantalla - $anchoLinea ) / 2;
+	$agregarEspacios = intdiv ( $anchoPantalla - $anchoLinea, 2 );
 	
 	$fn = fopen ( $archivoLogo , "r" );
 
